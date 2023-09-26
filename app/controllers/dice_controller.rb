3f0f251d78877c8d275151e7ac71da49e3f0a4c6
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-09-26 22:47:19 +0000

    Added some code

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,14 @@
+class DiceController < ApplicationController
+  def one
+  end
+  def two
+  end
+  def three
+  end
+  def four
+  end
+  def five
+  end
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+  get("/dice/2/6", { :controller => "dice", :action => "one" })
+  get("/dice/2/10", { :controller => "dice", :action => "two" })
+  get("/dice/1/20", { :controller => "dice", :action => "three" })
+  get("/dice/5/4", { :controller => "dice", :action => "four" })
+  get("/dynamic/:number_of_dice/6", { :controller => "dice", :action => "five" })
+  get("/", { :controller => "dice", :action => "home" })
 end
